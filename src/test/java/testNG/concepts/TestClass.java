2f0d5eed429a5860eab5aa58296f8e9192f66ae3
tetianaNEC <tetiana.nec@gmail.com>
2020-11-04 21:22:32 -0500

    New class added

---
+++ src/test/java/testNG/concepts/TestClass.java
@@ -0,0 +1,12 @@
+package testNG.concepts;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	
+	@Test
+	public void test() {
+		System.out.println("This class will be pushed to repo");
+	}
+}
